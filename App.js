7c659f17de1b0c4d05b85d715989cfb5e77a0225
Utkarsh Mishra <aditya@Adityas-MacBook-Air.local>
2024-04-13 02:20:39 +0530

    ep-2

--- App.js
+++ App.js
@@ -1,19 +1,5 @@
-/* 
-
-<div id="parent">
-  <div id=child>
-    <h1>I'm h1 Tag</h1>
-    <h2>I'm h2 Tag</h2>
-  </div>
-  <div id=child2>
-    <h1>I'm h1 Tag</h1>
-    <h2>I'm h2 Tag</h2>
-  </div>
-</div>
-
-ReactElement(Object) => HTML(Browser Understands)
-
-*/
+import React from "react";
+import ReactDOM from "react-dom";
 
 const parent = React.createElement(
     "div",
