61a17fdee573e2b555557399ad0775fd4bc9ac81
Utkarsh Mishra <aditya@Adityas-MacBook-Air.local>
2024-04-30 00:35:25 +0530

    ep-08

--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -3,13 +3,26 @@ import React from "react";
 class UserClass extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      count: 0,
+      count2: 0,
+    };
   }
 
   render() {
-    const {name, location} = this.props;
-    const {count, count2} = this.state;
+    const { name, location } = this.props;
+    const { count } = this.state;
     return (
       <div className="user-card">
+        <h1>Count : {count}</h1>
+        <button
+          onClick={() => {
+            this.setState({ count: this.state.count + 1 });
+          }}
+        >
+          Count Increase
+        </button>
         <h2>Name: {name}</h2>
         <h2>Location: {location}</h2>
         <h2>Contact: @utkarshmishra0708</h2>
